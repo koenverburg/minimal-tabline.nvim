52a8ca6741cfe037f847e5daf47e6a1d3665680f
Koen Verburg <creativekoen@gmail.com>
2022-06-07 16:52:48 +0200

    chore: moved highlights to vim file

--- lua/minimal-tabline/init.lua
+++ lua/minimal-tabline/init.lua
@@ -7,12 +7,12 @@ local SPACE    = " "
 local RESET    = "%#MTReset#"
 local ACTIVE   = "%#MTActive#"
 
-vim.cmd [[ hi MTActive    gui=UnderLinebold ctermfg=none ctermbg=none ]]
-vim.cmd [[ hi MTReset     gui=none          ctermfg=none ctermbg=none ]]
-
-vim.cmd [[ hi TabLine     cterm=none ctermbg=none gui=none ]]
-vim.cmd [[ hi TabLineSel  cterm=none ctermbg=none gui=none ]]
-vim.cmd [[ hi TabLineFill cterm=none ctermbg=none gui=none ]]
+-- vim.cmd [[ hi MTActive    gui=UnderLinebold ctermfg=none ctermbg=none ]]
+-- vim.cmd [[ hi MTReset     gui=none          ctermfg=none ctermbg=none ]]
+--
+-- vim.cmd [[ hi TabLine     cterm=none ctermbg=none gui=none ]]
+-- vim.cmd [[ hi TabLineSel  cterm=none ctermbg=none gui=none ]]
+-- vim.cmd [[ hi TabLineFill cterm=none ctermbg=none gui=none ]]
 
 local function minimal(options)
   local line = ""
